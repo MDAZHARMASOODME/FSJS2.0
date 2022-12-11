e4dfa4020ce7d5f5df3c7b7e244a249e3fd4134c
MDAZHARMASOODME <azharmasoodone11@gmail.com>
2022-12-11 14:47:00 +0530

    javascript

---
+++ javascript/sat_script.js
@@ -0,0 +1,38 @@
+// using let 
+// let firstname = 'MDAZHAR';
+// let lastname = 'Masood';
+// const  mobile  = 7903251078;
+
+// console.log('my first name ',firstname);
+// console.log('My Last Name is',lastname);
+// console.log(mobile, firstname,lastname);
+
+
+const z = 4;
+const y = 5;
+
+console.log (z+y); 
+
+console.log (z-y);
+console.log (z*y);
+console.log (z/y);
+
+console.log (z%y);
+//  operator 
+// 1.Assignment Operator 
+// 2.Arthimatic Operator 
+// 3.Comparision operator 
+
+let abc = 10;
+let xyz = 20;
+console.log( abc>xyz);
+console.log( xyz>abc);
+console.log(xyz>=abc);
+console.log(xyz<=abc)
+
+
+
+
+
+
+
