1af81ff80c858c2cf922dddb6225ddc8aeb258ae
MDAZHARMASOODME <azharmasoodone11@gmail.com>
2022-12-19 15:57:37 +0530

    update

---
+++ javascript/17dec_array.js
@@ -0,0 +1,26 @@
+// let cities = ['DHANBAD', 'Ranchi'];
+// let removecities =(cities.shift());
+// console.log(cities);
+// const cities = ['Manchester', 'Liverpool'];
+// cities.shift();
+// console.log(cities); 
+
+// let cities = ['DHANBAD', 'Ranchi','kolkata','Mumbai' ];
+// let index = cities.indexOf('Mumbai');
+// if (index !== -1) {
+//   cities.splice(index, 1);
+// }
+// console.log(cities); 
+// let cities = ['DHANBAD', 'Ranchi','kolkata','Mumbai' ];
+// let index = cities.indexOf('Ranchi');
+// if (index !== -1) {
+//   cities.splice(index, 4);
+// }
+// console.log(cities); 
+
+
+const cities = ['Parrot', 'Falcon', 'Owl'];
+
+for (const citie of cities) {
+  console.log(cities);
+}
